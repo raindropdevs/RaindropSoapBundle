52a1e111053dc895a144f046e531b340d8b29d60
Edoardo Rivello <edoardo.rivello@gmail.com>
2013-05-15 11:11:08 +0200

    add soap manager

---
+++ Soap/SoapManager.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Raindrop\SoapBundle\Soap;
+
+/**
+ * SoapManager
+ */
+class SoapManager implements SoapInterface
+{
+    /**
+     * @var SoapInterface
+     */
+    protected $soapClient;
+
+    public function __construct(SoapInterface $soapClient)
+    {
+        $this->soapClient = $soapClient;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function initialize($wsdl, array $parameters)
+    {
+        $this->soapClient->initialize($wsdl, $options);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function call($functionName, array $arguments)
+    {
+        return $this->soapClient->call($functionName, $arguments);
+    }
+}
